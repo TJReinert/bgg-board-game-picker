70ea66f2eaea75964261ad1128573345062a4514
Teddy Reinert <teddyreinert.github@gmail.com>
2023-08-13 18:01:35 -0500

    Remove template

--- bgg-board-game-picker/src/app/app.component.ts
+++ bgg-board-game-picker/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bgg-board-game-picker';
+  title = 'Board Game Geek Game Picker';
 }
